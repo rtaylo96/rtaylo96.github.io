bb0be1399bc4f0ef6320ce181778da624f9601ae
Rachel Taylor <rtaylo96@uncc.edu>
2021-09-29 22:02:13 -0400

    calculator_try upload

---
+++ itis3135/scripts/calculator_try.js
@@ -0,0 +1,9 @@
+function showNum(number){
+    alert("The number you pressed was " + number);
+}
+
+/** 
+ * I'm assuming this is all we had to do for the calculator because of how vague the instructions are
+ * I am however going to do the tutorial so I know for next time what it is I have to do. 
+ * Next time, I should try to implement actually calculating numbers that are inputted. 
+ */
