0db1c327e6135e5a50b2459a6db3a93af881875c
Rachel Taylor <rtaylo96@uncc.edu>
2021-11-02 18:35:07 -0400

    Activity 10

--- itis3135/scripts/jquery-slideshow.js
+++ itis3135/scripts/jquery-slideshow.js
@@ -1,4 +1,5 @@
 // js used for the slideshow, however it breaks without the script being in the html
+// the following code has been adapted from www.youtube.com/watch?v=iCBBLAR3R-60
 $(document).ready(function() {
     var imgs = new Array();
     var intervalID;
