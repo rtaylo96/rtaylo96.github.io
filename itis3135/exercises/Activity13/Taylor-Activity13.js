5255b566f4b23f23004a60e42ddb49f2fd8abee5
Rachel Taylor <rtaylo96@uncc.edu>
2021-11-18 14:59:11 -0500

    Activity 13

---
+++ itis3135/exercises/Activity13/Taylor-Activity13.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $.ajax({
+        type: "get",
+        url: "team.json", // changed from team.xml to team.json
+        beforeSend: function() {
+            $("#team").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json", // changed from xml to json
+        success: function(data) {
+            $("#team").html("");
+            // XML
+            // $(data).find("management").children().each(function() {
+            //    var xmlDoc = $(this);
+            //    $("#team").append
+            //    ("<h3>" + xmlDoc.find("name").text() + "</h3>" +
+            //          xmlDoc.find("title").text() + "<br>" +
+            //            xmlDoc.find("bio").text() + "<br>");
+            //});
+            
+            // JSON
+            $(data).find("management").children().each(function(){
+                var jsonDoc = $(this);
+                $("#team").append
+                ("<h3>" + jsonDoc.find("name").text() + "</h3>" +
+                        jsonDoc.find("title").text() + "<br>" +
+                        jsonDoc.find("bio").text() + "<br>");
+            });
+        }
+    });
+});
