b9bb959501516ec2aced015fd061d3e150ac5c1e
Rachel Taylor <rtaylo96@uncc.edu>
2021-11-24 02:04:55 -0500

    ahhhhhhh

--- itis3135/exercises/Activity16/Taylor-Activity16.js
+++ itis3135/exercises/Activity16/Taylor-Activity16.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // URL found at https://www.flickr.com/services/feeds/docs/photos_public/
-    var url = "https://www.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&tags=vectacorpbuilding&format=json&jsoncallback=?";
+    var url = "https://api.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding"
     $.getJSON(url, function(data){
         var html = "";
         $.each(data.items, function(i, item){
