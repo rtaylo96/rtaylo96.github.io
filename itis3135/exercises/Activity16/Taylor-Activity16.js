26a5215025686097e338495a00026d9227c3d605
Rachel Taylor <rtaylo96@uncc.edu>
2021-11-24 01:15:26 -0500

    Update 16

--- itis3135/exercises/Activity16/Taylor-Activity16.js
+++ itis3135/exercises/Activity16/Taylor-Activity16.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
     var url = "http://www.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
     $.getJSON(url, function(data){
         var html = "";
-        $.each(this, function(key, value){
-            html += "<a href= \" " + value.media.m + "\" data-lightbox = \"images\" data-title=" 
-            + value.title + "><img src=" + value.media.m + "></a>";
+        $.each(data.item, function(i, item){
+            html += "<a href= \" " + item.media.m + "\" data-lightbox = \"images\" data-title=" 
+            + item.title + "><img src=" + item.media.m + "></a>";
         });
         $("#new_building").html(html);
     });
